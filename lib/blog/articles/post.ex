5a3d25e0aba7a32afba13c4a1a3d7f3a678eb0dc
Mark-Gallacher <mark.g.gallacher@gmail.com>
2025-06-05 22:03:41 +0100

    add parsing to tag - to ensure consistency

--- lib/blog/articles/post.ex
+++ lib/blog/articles/post.ex
@@ -2,6 +2,25 @@ defmodule Blog.Articles.Post do
   @enforce_keys [:id, :title, :date, :description, :image, :body, :tags, :show]
   defstruct [:id, :title, :date, :description, :image, :body, :tags, :show]
 
+  @doc ~s"""
+  Ensure tags are in standard format
+
+  Converts "machine-learning" => "Machine Learning"
+  """
+  def parse_tags(attrs) when is_map(attrs) do
+    {tags, attrs} = Map.pop(attrs, :tags)
+
+    tags = tags
+    |> Enum.map(&String.split(&1, "-"))
+    |> Enum.map(fn list ->
+      list
+      |> Enum.map(&String.capitalize(&1))
+      |> Enum.join(" ")
+    end)
+
+    Map.merge(%{tags: tags}, attrs)
+  end
+
   @doc false
   def build(filename, attrs, body) do
     [year, day_month_id] =
@@ -13,6 +32,9 @@ defmodule Blog.Articles.Post do
     [day, month, id] = String.split(day_month_id, "-", parts: 3)
 
     date = Date.from_iso8601!("#{year}-#{month}-#{day}")
+
+    attrs = attrs 
+    |> parse_tags()
 
     struct!(__MODULE__, [id: id, date: date, body: body] ++ Map.to_list(attrs))
   end
